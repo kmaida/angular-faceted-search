6f45041a5cb59650535a56e859b3139f577b80af
Kim Maida <git@kim-maida.com>
2015-02-09 17:25:46 -0500

    * Optimize code order.

--- app.js
+++ app.js
@@ -120,8 +120,139 @@ myApp.controller('MainCtrl', function($scope, Helpers) {
 			studs: 0
 		}
 	];
+	
+	$scope.count = function (prop, value) {
+		return function (el) {
+			return el[prop] == value;
+		};
+	};
+	
+	// Sort the available facets alphabetically/numerically.
+	// http://stackoverflow.com/a/18261306
+	$scope.orderByValue = function(value) {
+		return value;
+	};
+	
+	/*---
+		FACET MANIPULATION FUNCTIONS
+			- clear facets, search
+			- FacetResults constructor
+	---*/
 
-	// Watch the facets that are selected.
+	// Reset all previously-selected facets.
+	$scope.clearAllFacets = function() {
+		$scope.activeFacets = [];
+		$scope.useFacets = {};
+	};
+
+	// Clear search query.
+	$scope.clearQuery = function() {
+		$scope.query = null;
+	};
+
+	// Clear a specific facet.
+	$scope.clearFacet = function(facet) {
+		// Find the index of the facet so we can remove it from the active facets.
+		var i = $scope.activeFacets.indexOf(facet);
+
+		// If it exists, remove the facet from the list of active facets.
+		if (i != -1) {
+			$scope.activeFacets.splice(i, 1);
+
+			// Find the corresponding facet in the filter models and turn it off.
+			for (var k in $scope.useFacets) {
+				if ($scope.useFacets[k]) {
+					$scope.useFacets[k][facet] = false;
+				}
+			}
+		}
+	};
+
+	// Clear any active facets when a search query is entered (or cleared).
+	// Add newValue && (!!oldValue === false) to if statement to allow search query to be changed and preserve facets.
+	$scope.$watch('query', function (newValue, oldValue) {
+		if ((newValue !== oldValue) && $scope.activeFacets.length) {
+			$scope.clearAllFacets();
+		}
+	});
+	
+	// FacetResults "constructor" object.
+	// http://davidwalsh.name/javascript-objects-deconstruction
+	var FacetResults = {
+		init: function(facetIndex, facetName) {
+			this.facetIndex = facetIndex;
+			this.facetName = facetName;
+		},
+		filterItems: function(filterAfterArray) {
+			// Name the new array created after filter is run.
+			var newArray = 'filterAfter_' + this.facetIndex;
+			// Attach the new array to the $scope.
+			$scope[newArray] = [];
+
+			selected = false;
+
+			// Iterate over previously filtered items.
+			for (var n in filterAfterArray) {
+				var itemObj = filterAfterArray[n],
+					useFacet = $scope.useFacets[this.facetName];
+
+				// Iterate over new facet.
+				for (var facet in useFacet) {
+					if (useFacet[facet]) {
+						selected = true;
+
+						// Push facet to list of active facets if doesn't already exist.
+						if (!Helpers.contains($scope.activeFacets, facet)) {
+							$scope.activeFacets.push(facet);
+						}
+
+						// Push item from previous filter to new array if matches new facet and unique.
+						// (Using == instead of === enables matching integers to strings)
+						if (itemObj[this.facetName] == facet && !Helpers.contains($scope[newArray], itemObj)) {
+							$scope[newArray].push(itemObj);
+							break;
+						}
+					}
+				}
+			}
+
+			if (!selected) {
+				$scope[newArray] = filterAfterArray;
+			}
+		}
+	};
+	
+	/*---
+		SET UP FACETS
+			- define facet group names
+			- compile all facets that belong in each group
+	---*/
+	
+	// Define the facet group names.
+	// If fetching from data, this will need to be in resolve/callback.
+	var facetGroupNames = ['type', 'color', 'studs'];
+	var facetGroupNamesLen = facetGroupNames.length;
+	$scope.facetGroups = [];
+
+	// Collect all options for each facet group from items dataset.
+	// The HTML template will iterate over the facetGroups array to generate filter options.
+	// (Alternately, we could pre-define the facets we want to use)
+	for (var i = 0; i < facetGroupNamesLen; i++) {
+		var facetGroupObj = {
+				name: facetGroupNames[i],
+				facets: Helpers.uniq($scope.items, facetGroupNames[i])
+			};
+
+		$scope.facetGroups.push(facetGroupObj);
+	}
+
+	/*---
+		WATCH FACET SELECTION
+			- "new" each facet results set
+			- run filters
+			- return final list of items after last filter is run
+	---*/
+	
 	$scope.$watch(function () {
 		return {
 			items: $scope.items,
@@ -130,135 +261,7 @@ myApp.controller('MainCtrl', function($scope, Helpers) {
 	}, function (value) {
 		var selected;
 
-		$scope.count = function (prop, value) {
-			return function (el) {
-				return el[prop] == value;
-			};
-		};
-
-	/*---
-		FACETS
-			- define facet group names
-			- fetch facets from data
-			- create active facets array
-			- facet manipulation functions
-	---*/
-
-		// Define the facet group names.
-		var facetGroupNames = ['type', 'color', 'studs'];
-		var facetGroupNamesLen = facetGroupNames.length;
-		$scope.facetGroups = [];
-
-		// Collect all options for each facet group from items dataset.
-		// The HTML template will iterate over the facetGroups array to generate filter options.
-		// (Alternately, we could pre-define the facets we want to use)
-		for (var i = 0; i < facetGroupNamesLen; i++) {
-			var facetGroupObj = {
-					name: facetGroupNames[i],
-					facets: Helpers.uniq($scope.items, facetGroupNames[i])
-				};
-
-			$scope.facetGroups.push(facetGroupObj);
-		}
-
 		$scope.activeFacets = [];
-
-		// Sort the available facets alphabetically/numerically.
-		// http://stackoverflow.com/a/18261306
-		$scope.orderByValue = function(value) {
-			return value;
-		};
-
-		// Reset all previously-selected facets.
-		$scope.clearAllFacets = function() {
-			$scope.activeFacets = [];
-			$scope.useFacets = {};
-		};
-
-		// Clear search query.
-		$scope.clearQuery = function() {
-			$scope.query = null;
-		};
-
-		// Clear a specific facet.
-		$scope.clearFacet = function(facet) {
-			// Find the index of the facet so we can remove it from the active facets.
-			var i = $scope.activeFacets.indexOf(facet);
-
-			// If it exists, remove the facet from the list of active facets.
-			if (i != -1) {
-				$scope.activeFacets.splice(i, 1);
-
-				// Find the corresponding facet in the filter models and turn it off.
-				for (var k in $scope.useFacets) {
-					if ($scope.useFacets[k]) {
-						$scope.useFacets[k][facet] = false;
-					}
-				}
-			}
-		};
-
-		// Clear any active facets when a search query is entered (or cleared).
-		// Add newValue && (!!oldValue === false) to if statement to allow search query to be changed and preserve facets.
-		$scope.$watch('query', function (newValue, oldValue) {
-			if ((newValue !== oldValue) && $scope.activeFacets.length) {
-				$scope.clearAllFacets();
-			}
-		});
-
-	/*---
-		FILTERING BY FACET
-			- "constructor" object definition
-			- "new" each facet results set
-			- run filters
-			- return final list of items after last filter is run
-	---*/
-
-		// FacetResults "constructor" object.
-		// http://davidwalsh.name/javascript-objects-deconstruction
-		var FacetResults = {
-			init: function(facetIndex, facetName) {
-				this.facetIndex = facetIndex;
-				this.facetName = facetName;
-			},
-			filterItems: function(filterAfterArray) {
-				// Name the new array created after filter is run.
-				var newArray = 'filterAfter_' + this.facetIndex;
-				// Attach the new array to the $scope.
-				$scope[newArray] = [];
-
-				selected = false;
-
-				// Iterate over previously filtered items.
-				for (var n in filterAfterArray) {
-					var itemObj = filterAfterArray[n],
-						useFacet = $scope.useFacets[this.facetName];
-
-					// Iterate over new facet.
-					for (var facet in useFacet) {
-						if (useFacet[facet]) {
-							selected = true;
-
-							// Push facet to list of active facets if doesn't already exist.
-							if (!Helpers.contains($scope.activeFacets, facet)) {
-								$scope.activeFacets.push(facet);
-							}
-
-							// Push item from previous filter to new array if matches new facet and unique.
-							// (Using == instead of === enables matching integers to strings)
-							if (itemObj[this.facetName] == facet && !Helpers.contains($scope[newArray], itemObj)) {
-								$scope[newArray].push(itemObj);
-								break;
-							}
-						}
-					}
-				}
-
-				if (!selected) {
-					$scope[newArray] = filterAfterArray;
-				}
-			}
-		};
 
 		// Create new object for each set of facet results (ie., like "new"ing).
 		for (var i = 0; i < facetGroupNamesLen; i++) {
